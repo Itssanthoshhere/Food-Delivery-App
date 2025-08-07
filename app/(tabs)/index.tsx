cb10c16f794220e96463f750be209b0422bf1e94
Santhosh VS <santhosh02vs@gmail.com>
2025-08-07 12:03:45 +0530

    Implement full auth, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import { FlatList, Image, Pressable, Text, TouchableOpacity, View} from "react-native";
+import {FlatList, Image, Pressable, Text, TouchableOpacity, View} from "react-native";
 import {Fragment} from "react";
 import cn from 'clsx';
 
 import CartButton from "@/Components/CartButton";
 import {images, offers} from "@/constants";
-// import useAuthStore from "@/store/auth.store";
+import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
-    // const { user } = useAuthStore();
+    const { user } = useAuthStore();
 
     return (
         <SafeAreaView className="flex-1 bg-white">
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,30 @@
 import {SplashScreen, Stack} from "expo-router";
-import {useFonts} from "expo-font";
-import {useEffect} from "react";
+import { useFonts } from 'expo-font';
+import { useEffect} from "react";
 
 import './globals.css';
+import * as Sentry from '@sentry/react-native';
+import useAuthStore from "@/store/auth.store";
 
-export default function RootLayout() {
+Sentry.init({
+    dsn: 'https://f49fad7cc2c2962e2a39252d05649976@o4509791949684736.ingest.us.sentry.io/4509801156247552',
+
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
+
+    // Configure Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+    const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
     const [fontsLoaded, error] = useFonts({
         "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
         "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -18,5 +38,13 @@ export default function RootLayout() {
         if(fontsLoaded) SplashScreen.hideAsync();
     }, [fontsLoaded, error]);
 
+    useEffect(() => {
+        fetchAuthenticatedUser()
+    }, []);
+
+    if(!fontsLoaded || isLoading) return null;
+
     return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
+
+Sentry.showFeedbackWidget();
